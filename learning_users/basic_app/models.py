a133065b20b1d4009158c94326c520275ef872bb
noman <nomanahmed93@gmail.com>
2018-05-21 05:02:49 +0530

    'first'

---
+++ learning_users/basic_app/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class UserProfileInfo(models.Model):
+
+    #Create relationship (don't inherit from User!)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+
+    #Add any additional attribute you want
+    portfolio_site = models.URLField(blank = True)
+    profile_pic = models.ImageField(upload_to = 'profile_pics', blank = True)
+
+    def __str__(self):
+        #Built-in attribute of django.contrib.aut.models.User!
+        return self.user.username
